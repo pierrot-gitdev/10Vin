5b3e3b6381bf53c56959ff3501100840163a2929
Pierre Robert <pierre@footbar.com>
2026-01-16 16:29:42 +0100

    Initial Commit

---
+++ 10Vin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  10Vin
+//
+//  Created by Pierre ROBERT on 16/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 10Vin/_0VinApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0VinApp.swift
+//  10Vin
+//
+//  Created by Pierre ROBERT on 16/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct _0VinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
